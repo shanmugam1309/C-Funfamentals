3abb48f52a32728296a67d05405df52dfa94d6e8
shanmugam1309 <shanmugaprianvelan2001@gmail.com>
2023-11-27 17:13:51 +0530

    add interview question practise

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -99,12 +99,43 @@ for (int i = 0; i < 5; i++)
 //}
 
 var numbers = new List<int> { 45, 56, 99, 48, 67, 78 };
+Console.WriteLine($"I found 99 at index{numbers.IndexOf(99)}");
 numbers.Sort();
 Console.WriteLine($"I found 99 at index{numbers.IndexOf(99)}");
 foreach (var number in numbers)
 {
-    Console.WriteLine($"{number}!");
+    Console.WriteLine($"{number}");
 }
+
+
+//List<int> scores = new List<int> { 97, 92, 81, 60 };
+
+//for (int i = 0; i < scores.Count; i++)
+//{
+//    if (scores[i] > 80)
+//    {
+//        Console.WriteLine($"Found a score > 80: {scores[i]}");
+//    }
+//}
+
+List<int> scores = new List<int> { 97, 92, 81, 60 };
+IEnumerable<string> scoreQuery =
+    from score in scores
+    where score > 80
+    orderby score descending
+    select $" The score is {score}";
+
+Console.WriteLine(scoreQuery.Count());
+
+foreach (string score in scoreQuery)
+{
+    Console.WriteLine(score);
+}
+
+
+
+
+
 
 //OOPS CONCEPT
 
@@ -118,4 +149,127 @@ class car
         Console.WriteLine(carobj.color1);
     }
 }
+
+
+// Types of Constructor 
+
+// 1. Default constructor
+
+public class MyClass
+{
+    // Default Constructor
+    public MyClass()
+    {
+        // Initialization code (if needed)
+    }
+}
+
+// 2. Parameterized constructor
+
+public class Car
+{
+    public string Make;
+    public string Model;
+
+    // Parameterized Constructor
+    public Car(string make, string model)
+    {
+        Make = make;
+        Model = model;
+    }
+}
+
+// 3. Copy constructor
+
+public class Person
+{
+    public string Name;
+    public int Age;
+
+    // Copy Constructor (using a method)
+    public static Person CopyPerson(Person original)
+    {
+        return new Person { Name = original.Name, Age = original.Age };
+    }
+}
+
+// 4. Static constructor
+
+public class Myglas
+{
+    // Static Constructor
+    static Myglas()
+    {
+        // Initialization code for static members
+    }
+}
+
+// 5. Private constructor
+
+public class Singleton
+{
+    private static Singleton instance;
+
+    // Private Constructor
+    private Singleton()
+    {
+        // Initialization code
+    }
+
+    public static Singleton GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
+// Session in dot net
+
+
+public partial class SessionExample : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            // Set a value in session state
+            Session["UserName"] = "JohnDoe";
+        }
+
+        // Retrieve the value from session state
+        string userName = Session["UserName"] as string;
+
+        object lblUserName = null;
+        // Display the value in a label on the web page
+        lblUserName.Text = $"Welcome, {userName}!";
+    }
+}
+
+
+// Boxing in dot net 
+
+
+
+class Programm
+{
+    static void Main()
+    {
+        // Example of boxing
+        int intValue = 42; // This is a value type (int)
+        object boxedObject = intValue; // Boxing: int to object
+
+        // Example of unboxing
+        int unboxedValue = (int)boxedObject; // Unboxing: object to int
+
+        // Display results
+        Console.WriteLine($"Original Value (int): {intValue}");
+        Console.WriteLine($"Boxed Value (object): {boxedObject}");
+        Console.WriteLine($"Unboxed Value (int): {unboxedValue}");
+    }
+}
+
+
 
